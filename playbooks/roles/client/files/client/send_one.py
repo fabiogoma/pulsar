ff8d711f9468116ba3a14386545da3c0db48b156
Fabio G. Martins <fabiom@ansible.europe.intranet>
2020-09-08 17:22:29 +0200

    Initial commit

---
+++ playbooks/roles/client/files/client/send_one.py
@@ -0,0 +1,18 @@
+import pulsar
+import uuid
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config.read_file(open("{}/config.ini".format(os.path.dirname(__file__))))
+
+broker_address = config['default'].get('broker_address')
+topic_name  = config['default'].get('topic_name')
+messages_per_second = config['default'].getint('messages_per_second')
+
+client = pulsar.Client(service_url=broker_address, operation_timeout_seconds=500)
+producer = client.create_producer(topic=topic_name, send_timeout_millis=500000, max_pending_messages=10000)
+
+producer.send(str(uuid.uuid4()).encode('utf-8'))
+
+client.close()
