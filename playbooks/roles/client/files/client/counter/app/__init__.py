a4884cdbb5ce1666b2d92388eea604add69e8084
Fabio G. Martins <fabiom@ansible.europe.intranet>
2020-09-14 13:15:52 +0200

    Add error handling

--- playbooks/roles/client/files/client/counter/app/__init__.py
+++ playbooks/roles/client/files/client/counter/app/__init__.py
@@ -7,36 +7,60 @@ app = Flask(__name__)
 
 @app.route('/counter/in', methods=['PUT'])
 def published_message():
-    client.incr('published', 1)
-    out = int(client.get('published').decode('utf-8'))
+    out = {}
+    try:
+        client.incr('published', 1)
+        out = int(client.get('published').decode('utf-8'))
+    except OSError as err:
+        out = { "Error": "OS error: {0}".format(err) }
+
     return jsonify(total_published=out)
 
 @app.route('/counter/out', methods=['PUT'])
 def consumed_message():
-    client.incr('consumed', 1)
-    out = int(client.get('consumed').decode('utf-8'))
+    out = {}
+    try:
+        client.incr('consumed', 1)
+        out = int(client.get('consumed').decode('utf-8'))
+    except OSError as err:
+        out = { "Error": "OS error: {0}".format(err) }
+
     return jsonify(total_consumed=out)
 
 @app.route('/counter/total', methods=['GET'])
 def total_message():
     total_published = 0
     total_consumed = 0
+    total_message_dict = {}
 
-    total_published = int(client.get('published').decode('utf-8'))
-    total_consumed = int(client.get('consumed').decode('utf-8'))
-
-    total_message_dict = { "total_published": total_published, "total_consumed": total_consumed }
+    try:
+        total_published = int(client.get('published').decode('utf-8'))
+        total_consumed = int(client.get('consumed').decode('utf-8'))
+        total_message_dict = { "total_published": total_published, "total_consumed": total_consumed }
+    except OSError as err:
+        total_message_dict = { "Error": "OS error: {0}".format(err) }
+    except AttributeError as err:
+        client.set('published', str(total_published))
+        client.set('consumed', str(total_consumed))
+        total_published = int(client.get('published').decode('utf-8'))
+        total_consumed = int(client.get('consumed').decode('utf-8'))
+        total_message_dict = { "total_published": total_published, "total_consumed": total_consumed }
 
     return jsonify(total_messages=total_message_dict)
 
 @app.route('/counter/reset', methods=['POST'])
 def reset_message():
-    client.set('published', '0')
-    client.set('consumed', '0')
+    total_message_dict = {}
 
-    total_published = int(client.get('published').decode('utf-8'))
-    total_consumed = int(client.get('consumed').decode('utf-8'))
+    try:
+        client.set('published', '0')
+        client.set('consumed', '0')
 
-    total_message_dict = { "total_published": total_published, "total_consumed": total_consumed }
+        total_published = int(client.get('published').decode('utf-8'))
+        total_consumed = int(client.get('consumed').decode('utf-8'))
+
+        total_message_dict = { "total_published": total_published, "total_consumed": total_consumed }
+    except OSError as err:
+        total_message_dict = { "Error": "OS error: {0}".format(err) }
 
     return jsonify(total_messages=total_message_dict)
